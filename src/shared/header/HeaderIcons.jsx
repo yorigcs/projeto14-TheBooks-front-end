620e83b0a6cc069ad539ba7fad15ef756b5ec676
Yorí <yorigcsdev@gmail.com>
2022-07-06 21:09:23 -0300

    Feature: change header icons && add functionality to profile and listOptions

--- src/shared/header/HeaderIcons.jsx
+++ src/shared/header/HeaderIcons.jsx
@@ -1,24 +1,64 @@
 import styled from "styled-components";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { HiShoppingCart } from "react-icons/hi";
-import { BsList } from "react-icons/bs";
-import { FaUserCircle } from "react-icons/fa";
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ListIcon from '@mui/icons-material/List';
 
 
-const HeaderIcons = () => {
+const HeaderIcons = ({ setRenderProfile, setRenderOptions }) => {
+    const [colorProfile, setColorProfile] = useState(false);
+    const [colorOptions, setColorOptions] = useState(false);
+
+    const handleProfile = () => {
+        setRenderProfile(profile => !profile);
+        setRenderOptions(false);
+        setColorProfile(!colorProfile);
+    }
+
+    const handleOptions = () => {
+        setRenderOptions(options => !options);
+        setRenderProfile(false);
+        setColorOptions(!colorOptions);
+    }
+
+
     return (
-        <IconStyle>
-            <FaUserCircle size="2em" color="white" />
-            <HiShoppingCart size="2em" color="white" />
-            <BsList size="2em" color="white" />
-        </IconStyle>
+        <IconContainer>
+            <IconStyle changeColor={colorProfile} onClick={handleProfile}>
+                <PersonIcon sx={{ fontSize: 28 }} />
+            </IconStyle>
+
+            <IconStyle>
+                <ShoppingCartIcon sx={{ fontSize: 28 }} />
+            </IconStyle>
+
+            <IconStyle changeColor={colorOptions} onClick={handleOptions}>
+                <ListIcon sx={{ fontSize: 28 }} />
+            </IconStyle>
+
+        </IconContainer>
     )
 }
 
-const IconStyle = styled.div`
+const IconContainer = styled.div`
     display: flex;
     align-items:center;
     gap: 15px;
 `;
+
+const IconStyle = styled.div`
+    color: ${props => props.changeColor ? "blue" : "white"};
+    cursor: pointer;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    height: 2.5rem;
+    width: 2.5rem;
+    border-radius: 50%;
+    background-color: black;
+`;
+
+
 
 export default HeaderIcons;
