aab89fd5bef750defc4f84a20b4434441184e1b4
Yorí <yorigcsdev@gmail.com>
2022-07-11 17:21:54 -0300

    Change API link to heroku

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosI = axios.create(
     {
-        baseURL: "http://localhost:5000"
+        baseURL: "https://the-books-store-driven.herokuapp.com"
     }
 )
 export default axiosI;
